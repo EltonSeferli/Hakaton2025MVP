9848b437ca5505c65cc28c9d932d0fd4cd9bb7f4
unknown <eltonseferli2@gmail.com>
2025-09-22 13:11:35 +0400

    123

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,10 +4,6 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <div className={styles.footer}>
-      <img
-        src="src\assets\WhatsApp_Image_2025-09-13_at_23.47.46_947ce478-removebg-preview.png"
-        alt=""
-      />
       <h2>NC Engine</h2>
     </div>
   );
